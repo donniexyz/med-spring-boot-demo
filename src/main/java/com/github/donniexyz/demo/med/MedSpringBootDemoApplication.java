d5f878b496d4012baafd0cd5ea5498d38fa36c0e
donnie <6776930+donniexyz@users.noreply.github.com>
2024-05-30 19:13:48 +0800

    Init the project

---
+++ src/main/java/com/github/donniexyz/demo/med/MedSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.donniexyz.demo.med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/donniexyz/demo/med/MedSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.donniexyz.demo.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
