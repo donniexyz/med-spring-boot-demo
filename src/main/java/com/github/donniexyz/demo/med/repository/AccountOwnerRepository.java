2d578567df8ed757bf1db4f380fe794157e1eacd
donnie <6776930+donniexyz@users.noreply.github.com>
2024-06-01 21:54:31 +0800

    Change resource path to use default (/accountOwners)

--- src/main/java/com/github/donniexyz/demo/med/repository/AccountOwnerRepository.java
+++ src/main/java/com/github/donniexyz/demo/med/repository/AccountOwnerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(path = "accountOwner")
+@RepositoryRestResource()
 public interface AccountOwnerRepository extends JpaRepositoryImplementation<AccountOwner, Long> {
 }
