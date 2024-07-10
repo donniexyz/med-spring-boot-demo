7e4169357697a8d13c2023693af1948426cacc2e
donnie <6776930+donniexyz@users.noreply.github.com>
2024-07-10 22:54:17 +0800

    add on pom.xml jackson-datatype-jsr310 and objectMapper register module JavaTimeModule

--- src/main/java/com/github/donniexyz/demo/med/config/MedDemoApplicationConfiguration.java
+++ src/main/java/com/github/donniexyz/demo/med/config/MedDemoApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.github.donniexyz.demo.med.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.donniexyz.demo.med.converter.MonetaryAmountDeserializer;
 import com.github.donniexyz.demo.med.converter.MonetaryAmountSerializer;
 import org.jetbrains.annotations.NotNull;
@@ -27,6 +28,7 @@ public class MedDemoApplicationConfiguration {
     public static ObjectMapper registerOurModules(ObjectMapper objectMapper, boolean serMonetaryAmountAsString) {
         objectMapper.registerModule(new Hibernate6Module());
         objectMapper.registerModule(new MoneyModule());
+        objectMapper.registerModule(new JavaTimeModule());
         // TODO: write unit test that make sure the custom ser & deser is used by objectMapper instead of MoneyModule's
         SimpleModule module = new SimpleModule("MonetaryAmountAsStringModule");
         module.addDeserializer(MonetaryAmount.class, new MonetaryAmountDeserializer());
